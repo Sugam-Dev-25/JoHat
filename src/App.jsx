e1c5b3dc1b4aeb6f9f53b2a985c36235d794dc02
Sugam-Dev-25 <sugamkarmakar25@gmail.com>
2026-03-23 14:47:02 +0530

    Johat Project First Commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+// import "./App.css";
+
+import Header from "./Components/Layouts/Header/Header";
+import Footer from "./Components/Layouts/Footer/Footer";
+
+import HomeComponents from "./Components/Home/HomeComponents";
+import AboutComponents from "./Components/About/AboutComponents";
+import ServiceComponents from "./Components/Service/ServiceComponents";
+import ContactComponents from "./Components/Contact/ContactComponents";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomeComponents />} />
+        <Route path="/about" element={<AboutComponents />} />
+        <Route path="/service" element={<ServiceComponents />} />
+        <Route path="/contact" element={<ContactComponents />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Service/ServiceComponents.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ServiceComponents = () => {
+  return (
+    <div>ServiceComponents</div>
+  )
+}
+
+export default ServiceComponents
